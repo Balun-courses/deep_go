5df4ab4833c64631ff4d0d3d5650586d6a1e1921
Max Pryakhin <maxim.k.pryakhin@gmail.com>
2024-10-31 12:29:18 +0300

    implement ToLittleEndian

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,7 +10,34 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	pointer := unsafe.Pointer(&number)
+	var result uint32
+	resultPointer := unsafe.Pointer(&result)
+
+	for i := 0; i < 4; i++ {
+		byteValue := *(*int8)(unsafe.Add(pointer, i))
+		*(*int8)(unsafe.Add(resultPointer, 3-i)) = byteValue
+	}
+
+	return result
+}
+
+func ToLittleEndianGeneric[T any](number T) T {
+	size := SizeOf(number)
+	pointer := unsafe.Pointer(&number)
+	var result T
+	resultPointer := unsafe.Pointer(&result)
+
+	for i := 0; i < int(size); i++ {
+		byteValue := *(*int8)(unsafe.Add(pointer, i))
+		*(*int8)(unsafe.Add(resultPointer, int(size)-1-i)) = byteValue
+	}
+
+	return result
+}
+
+func SizeOf[T any](v T) uintptr {
+	return unsafe.Sizeof(v)
 }
 
 func TestSerializationProperties(t *testing.T) {
@@ -42,6 +70,111 @@ func TestSerializationProperties(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestSerializationGenericUint64Properties(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGeneric(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestSerializationGenericUint16Properties(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0xFF00,
+			result: 0x00FF,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGeneric(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestSerializationGenericUint32Properties(t *testing.T) {
+	tests := map[string]struct {
+		number uint32
+		result uint32
+	}{
+		"test case #1": {
+			number: 0x00000000,
+			result: 0x00000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFF,
+			result: 0xFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF,
+			result: 0xFF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF,
+			result: 0xFFFF0000,
+		},
+		"test case #5": {
+			number: 0x01020304,
+			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGeneric(test.number)
 			assert.Equal(t, test.result, result)
 		})
 	}
