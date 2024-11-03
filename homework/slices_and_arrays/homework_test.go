162eed1193e6ab208b1de93904d0b28ab2ac698a
Armenqaa <130700638+Armenqaa@users.noreply.github.com>
2024-11-03 18:06:02 +0300

    Add homework_2

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -11,35 +11,53 @@ import (
 
 type CircularQueue struct {
 	values []int
-	// need to implement
+	start  int
+	filled int
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{make([]int, size), 0, 0}
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+	q.values[(q.start+q.filled)%len(q.values)] = value
+	q.filled++
+	return true
+
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.Empty() {
+		return false
+	}
+	q.start++
+	q.filled--
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+	return q.values[q.start%len(q.values)]
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+	return q.values[(q.start+q.filled)%len(q.values)]
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.filled == 0
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	return q.filled == len(q.values)
 }
 
 func TestCircularQueue(t *testing.T) {
