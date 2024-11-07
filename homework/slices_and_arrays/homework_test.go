b4678e2e56b5d0ebc3d923698202bb4dadda56d7
kashipov.artem <kashipov.artem@wb.ru>
2024-11-07 16:30:01 +0400

    home2

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -12,34 +13,69 @@ import (
 type CircularQueue struct {
 	values []int
 	// need to implement
+	start int
+	end   int
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	if size <= 0 {
+		fmt.Println("Default value for size will be applied")
+		size = 10
+	}
+	return CircularQueue{
+		values: make([]int, size),
+		end:    -1,
+	} // need to implement
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+	q.values[q.start] = value
+	q.start = (q.start + 1) % cap(q.values)
+	if q.Empty() {
+		q.end = q.start
+	}
+	return true
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.end == -1 {
+		return false
+	}
+	q.end = (q.end + 1) % cap(q.values)
+	if q.Full() {
+		q.end = -1
+	}
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.end == -1 {
+		return -1
+	}
+	v := (q.end - 1 + cap(q.values)) % cap(q.values)
+	return q.values[v]
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.end == -1 {
+		return -1
+	}
+	v := (q.start - 1 + cap(q.values)) % cap(q.values)
+	return q.values[v]
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.end == -1
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	if q.end != -1 && (q.start+1)%cap(q.values) == q.end {
+		return true
+	}
+	return false
 }
 
 func TestCircularQueue(t *testing.T) {
@@ -83,4 +119,29 @@ func TestCircularQueue(t *testing.T) {
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
+}
+
+func TestCircularQueueOne(t *testing.T) {
+	const queueSize = 1
+	queue := NewCircularQueue(queueSize)
+
+	assert.True(t, queue.Empty())
+	assert.False(t, queue.Full())
+
+	assert.Equal(t, -1, queue.Front())
+	assert.Equal(t, -1, queue.Back())
+	assert.False(t, queue.Pop())
+
+	assert.True(t, queue.Push(1))
+	assert.False(t, queue.Push(2))
+	assert.False(t, queue.Push(3))
+	assert.False(t, queue.Push(4))
+
+	assert.True(t, queue.Pop())
+	assert.True(t, queue.Empty())
+	assert.False(t, queue.Full())
+	assert.True(t, queue.Push(2))
+	assert.Equal(t, 2, queue.Front())
+	assert.Equal(t, 2, queue.Back())
+	assert.False(t, queue.Push(3))
 }
