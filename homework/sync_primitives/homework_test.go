18bcf2673f4b08cecd81d4714ff4a08905570734
VladimirBalun <vladimirbalun@yandex.ru>
2024-12-20 12:22:20 +0300

    fix: fix bug in homework by sync primitives

--- homework/sync_primitives/homework_test.go
+++ homework/sync_primitives/homework_test.go
@@ -48,11 +48,6 @@ func TestRWMutexWithWriter(t *testing.T) {
 		mutualExlusionWithReader.Store(false)
 	}()
 
-	go func() {
-		mutex.RLock() // another reader
-		mutualExlusionWithReader.Store(false)
-	}()
-
 	time.Sleep(time.Second)
 	assert.True(t, mutualExlusionWithWriter.Load())
 	assert.True(t, mutualExlusionWithReader.Load())
@@ -64,25 +59,65 @@ func TestRWMutexWithReaders(t *testing.T) {
 
 	var mutualExlusionWithWriter atomic.Bool
 	mutualExlusionWithWriter.Store(true)
-	var mutualExlusionWithReader atomic.Bool
-	mutualExlusionWithReader.Store(false)
 
 	go func() {
 		mutex.Lock() // another writer
 		mutualExlusionWithWriter.Store(false)
 	}()
 
+	time.Sleep(time.Second)
+	assert.True(t, mutualExlusionWithWriter.Load())
+}
+
+func TestRWMutexMultipleReaders(t *testing.T) {
+	var mutex RWMutex
+	mutex.RLock() // reader
+
+	var readersCount atomic.Int32
+	readersCount.Add(1)
+
 	go func() {
 		mutex.RLock() // another reader
-		mutualExlusionWithReader.Store(true)
+		readersCount.Add(1)
 	}()
 
 	go func() {
 		mutex.RLock() // another reader
-		mutualExlusionWithReader.Store(true)
+		readersCount.Add(1)
 	}()
 
 	time.Sleep(time.Second)
+	assert.Equal(t, int32(3), readersCount.Load())
+}
+
+func TestRWMutexWithWriterPriority(t *testing.T) {
+	var mutex RWMutex
+	mutex.RLock() // reader
+
+	var mutualExlusionWithWriter atomic.Bool
+	mutualExlusionWithWriter.Store(true)
+	var readersCount atomic.Int32
+	readersCount.Add(1)
+
+	go func() {
+		mutex.Lock() // another writer is waiting for reader
+		mutualExlusionWithWriter.Store(false)
+	}()
+
+	time.Sleep(time.Second)
+
+	go func() {
+		mutex.RLock() // another reader is waiting for a higher priority writer
+		readersCount.Add(1)
+	}()
+
+	go func() {
+		mutex.RLock() // another reader is waiting for a higher priority writer
+		readersCount.Add(1)
+	}()
+
+	time.Sleep(time.Second)
+
 	assert.True(t, mutualExlusionWithWriter.Load())
-	assert.False(t, mutualExlusionWithReader.Load())
+	assert.Equal(t, int32(1), readersCount.Load())
 }
