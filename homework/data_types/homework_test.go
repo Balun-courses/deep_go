ad5817422fe66d06a2939fd45231b561640ab3ea
yura.parshutkin <yura.parshutkin@gmail.com>
2025-03-12 09:00:23 +0200

    implement function to convert big endian to little endian

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"unsafe"
 )
 
-// go test -v homework_test.go
+type Unsigned interface {
+	~uint16 | ~uint32 | ~uint64
+}
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+// go test -v homework_test.go
+func ToLittleEndian[T Unsigned](number T) T {
+	var out T
+	n := int(unsafe.Sizeof(out))
+	for i := 0; i < n; i++ {
+		os := 8 * i
+		o := byte(number >> os & 0xFF)
+		ys := 8 * (n - i - 1)
+		out = out | T(o)<<ys
+	}
+	return out
 }
 
 func TestСonversion(t *testing.T) {
@@ -36,6 +47,76 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversionUint16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversionUint64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
 		},
 	}
 
