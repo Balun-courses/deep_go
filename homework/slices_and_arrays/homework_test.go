d8909511923fe3a7e96056d915a96464ad81c68e
Casey <casey.light@mail.ru>
2025-03-16 21:17:23 +0700

    hw2

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -11,35 +11,79 @@ import (
 
 type CircularQueue struct {
 	values []int
-	// need to implement
+	front  int
+	rear   int
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{
+		values: make([]int, size),
+		front:  -1,
+		rear:   -1,
+	}
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+
+	if q.front == -1 {
+		q.front = 0
+	}
+	q.rear = (q.rear + 1) % len(q.values)
+	q.values[q.rear] = value
+
+	return true
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.Empty() {
+		return false
+	}
+
+	if q.front == q.rear {
+		q.values[q.front] = 0
+		q.front = -1
+		q.rear = -1
+	} else {
+		q.values[q.front] = 0
+		q.front = (q.front + 1) % len(q.values)
+	}
+
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	return q.values[q.front]
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	return q.values[q.rear]
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.front == -1
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	if q.front == 0 && q.rear == len(q.values)-1 {
+		return true
+	}
+
+	if q.front == (q.rear+1)%len(q.values) {
+		return true
+	}
+
+	return false
 }
 
 func TestCircularQueue(t *testing.T) {
