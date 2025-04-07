64571b06e7d220d8648a9ea1871082c57fff59b6
Casey <casey.light@mail.ru>
2025-04-07 11:44:19 +0700

    hw7

--- homework/interfaces/homework_test.go
+++ homework/interfaces/homework_test.go
@@ -17,19 +17,30 @@ type MessageService struct {
 	// not need to implement
 	NotEmptyStruct bool
 }
+type Db struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
 
 type Container struct {
-	Services map[string]any
+	Services          map[string]any
+	SingleTonServices map[string]bool
 }
 
 func NewContainer() *Container {
 	return &Container{
-		Services: map[string]any{},
+		Services:          map[string]any{},
+		SingleTonServices: map[string]bool{},
 	}
 }
 
 func (c *Container) RegisterType(name string, constructor any) {
 	c.Services[name] = constructor
+}
+
+func (c *Container) RegisterSingletonType(name string, constructor any) {
+	c.Services[name] = constructor
+	c.SingleTonServices[name] = true
 }
 
 func (c *Container) Resolve(name string) (any, error) {
@@ -38,15 +49,38 @@ func (c *Container) Resolve(name string) (any, error) {
 		return nil, fmt.Errorf("service %s not found", name)
 	}
 
-	if service, ok := value.(func() any); !ok {
-		return nil, fmt.Errorf("service %s not a function", name)
-	} else {
-		return service(), nil
+	service, ok := value.(func() any)
+	if !ok {
+		if c.SingleTonServices[name] {
+			return c.Services[name], nil
+		}
+
+		return nil, fmt.Errorf("service %s not a constructor", name)
 	}
+
+	if c.SingleTonServices[name] {
+		c.Services[name] = service()
+		return c.Services[name], nil
+	}
+
+	return service(), nil
 }
 
 func TestDIContainer(t *testing.T) {
 	container := NewContainer()
+
+	container.RegisterSingletonType("Db", func() interface{} {
+		return &Db{}
+	})
+
+	db1, err := container.Resolve("Db")
+	assert.NoError(t, err)
+
+	db2, err := container.Resolve("Db")
+	assert.NoError(t, err)
+
+	assert.True(t, db1 == db2)
+
 	container.RegisterType("UserService", func() interface{} {
 		return &UserService{}
 	})
