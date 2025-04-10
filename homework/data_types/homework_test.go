43ca4ceaa0580fdd8ad7e4db4ffd3d163ed39d08
Kirill Danilov <danilov.kirill13@wb.ru>
2025-04-10 15:12:07 +0400

    add homework 1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T uint16 | uint32 | uint64](number T) T {
+	const mask = 0xFF                  // initialize bit mask for one byte
+	size := int(unsafe.Sizeof(number)) // number of bytes
+	var res T                          // initialize result of type T
+	for i := 0; i < size; i++ {        // iterate over bytes from higher order to lower
+		res <<= 8            // take all the bytes and make them higher by one order, leaving zero at the lowest byte
+		res |= number & mask // take the lowest byte from number and put in into the lowest byte of result
+		number = number >> 8 // remove the lowest byte from number, shift right and put the next highest byte to the lowest position
+	}
+	return res
 }
 
 func TestСonversion(t *testing.T) {
