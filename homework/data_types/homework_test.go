5d6cbda79d7a7c74d210c40334cd311f938682c8
Константин Жихарев <zhiharev.k@wb.ru>
2025-04-11 22:25:43 +0300

    task-1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T uint16 | uint32 | uint64 | uint](number T) T {
+	var (
+		size = unsafe.Sizeof(number)
+		ptr1 = unsafe.Pointer(&number)
+		ptr2 = unsafe.Add(unsafe.Pointer(&number), size-1)
+	)
+
+	for range int(size) / 2 {
+		*(*byte)(ptr1), *(*byte)(ptr2) = *(*byte)(ptr2), *(*byte)(ptr1)
+
+		ptr1 = unsafe.Add(ptr1, 1)
+		ptr2 = unsafe.Add(ptr2, -1)
+	}
+
+	return number
 }
 
 func TestСonversion(t *testing.T) {
