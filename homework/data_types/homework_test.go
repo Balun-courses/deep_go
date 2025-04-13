6b731637fb745b26f934b19d2d37364f3b09efb2
NIck <1405923+KiboMibo@users.noreply.github.com>
2025-04-13 14:16:36 +0300

    hw1 data types

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -4,13 +4,31 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/constraints"
 )
 
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-		
-	return 0 // need to implement
+	// побайтовый сдвиг, старший становится младшим младший - старшим
+	// используем маску 0xFF00000 и операцию сложения для выбора байта
+	return ((number & 0xFF000000) >> 24) |
+		((number & 0x00FF0000) >> 8) |
+		((number & 0x0000FF00) << 8) |
+		((number & 0x000000FF) << 24)
+}
+
+func ToLittleEndianGeneric[T constraints.Unsigned](number T) T {
+	// Так как uint8, uint16 не может быть сдвинут на 24, то конвертируем в больший тип,
+	// зетем приводим обратно к нужному типу
+	// выдаст ошибку на uint64, чтобы сработало нужно увеличить количество сдвигов
+	x := uint32(number)
+	x = ((x & 0xFF000000) >> 24) |
+		((x & 0x00FF0000) >> 8) |
+		((x & 0x0000FF00) << 8) |
+		((x & 0x000000FF) << 24)
+
+	return T(x)
 }
 
 func TestСonversion(t *testing.T) {
@@ -42,7 +60,7 @@ func TestСonversion(t *testing.T) {
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			result := ToLittleEndian(test.number)
+			result := ToLittleEndianGeneric(test.number)
 			assert.Equal(t, test.result, result)
 		})
 	}
