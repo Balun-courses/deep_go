1611bf082f04d8ec96efc516ca6af0cc9351cd3d
shunevich.a <shunevich.a@wildberries.work>
2025-04-14 16:06:24 +0300

    hw1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,7 +10,27 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	shift0 := number
+	shift8 := number >> 8
+	shift16 := number >> 16
+	shift24 := number >> 24
+
+	pointerToFirstByteOfShift0 := (*uint8)(unsafe.Pointer(&shift0))
+	pointerToFirstByteOfShift8 := (*uint8)(unsafe.Pointer(&shift8))
+	pointerToFirstByteOfShift16 := (*uint8)(unsafe.Pointer(&shift16))
+	pointerToFirstByteOfShift24 := (*uint8)(unsafe.Pointer(&shift24))
+
+	var res uint32 = 0
+
+	res += uint32(*pointerToFirstByteOfShift0)
+	res = res << 8
+	res += uint32(*pointerToFirstByteOfShift8)
+	res = res << 8
+	res += uint32(*pointerToFirstByteOfShift16)
+	res = res << 8
+	res += uint32(*pointerToFirstByteOfShift24)
+
+	return res
 }
 
 func TestСonversion(t *testing.T) {
