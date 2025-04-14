286134eba39f00ba3fe8ec7354dbaaf71cf76306
VladimirBalun <vladimirbalun@yandex.ru>
2025-04-14 18:30:14 +0300

    feat: add new material by strings

--- lessons/strings/leak_with_string/main.go
+++ lessons/strings/leak_with_string/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+)
+
+func printAllocs() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("%d MB\n", m.Alloc/1024/1024)
+}
+
 func findSequence(data string) string {
 	for i := 0; i < len(data)-1; i++ {
 		if data[i] == '\n' && data[i+1] == '\t' {
@@ -10,10 +22,17 @@ func findSequence(data string) string {
 	return ""
 }
 
-func processBigData() {
-	var data string
+func main() {
+	data := make([]byte, 1<<30)
 	// let's imagine that data was read from a file
 
-	sequence := findSequence(data)
+	str := unsafe.String(unsafe.SliceData(data), len(data))
+	sequence := findSequence(str)
 	_ = sequence // using of sequence later
+
+	printAllocs()
+	runtime.GC()
+	printAllocs()
+
+	runtime.KeepAlive(data)
 }
