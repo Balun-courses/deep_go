e6c92f7a09ffde88f0f1c23b6bb6ebb60dcf4c62
WaRDrayv <67011639+WaRDrayv@users.noreply.github.com>
2025-04-14 07:57:52 +0000

    generic func have problem with last 0 in hex

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -46,6 +48,103 @@ func TestСonversion(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			result := ToLittleEndian(test.number)
 			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+// ============== STAR !!!!! ==================//
+
+// Encode big endian to little endian for one of the following types: uint16 | uint32 | uint 64
+func ToLittleEndianAnyUint[T uint16 | uint32 | uint64](number T) T {
+	size := unsafe.Sizeof(number) //2, 4, 8
+	num := uint64(number)
+	result := ((num & 0xFF00000000000000) >> 56) |
+		((num & 0x00FF000000000000) >> 40) |
+		((num & 0x0000FF0000000000) >> 24) |
+		((num & 0x000000FF00000000) >> 8) |
+		((num & 0x00000000000000FF) << 56) |
+		((num & 0x000000000000FF00) << 40) |
+		((num & 0x0000000000FF0000) << 24) |
+		((num & 0x00000000FF000000) << 8)
+
+	if size == 2 {
+		result = result >> 48
+	}
+	if size == 4 {
+		result = result >> 32
+	}
+
+	return T(result)
+}
+
+func TestСonversion16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0x0F0F,
+			result: 0xF0F0,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x2010,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianAnyUint[uint16](test.number)
+			assert.Equal(t, test.result, result)
+			fmt.Printf("\n%04x\n\n", uint16(result))
+		})
+	}
+}
+
+func TestСonversion64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x8070605040302010,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianAnyUint[uint64](test.number)
+			assert.Equal(t, test.result, result)
+			fmt.Printf("\n%016x\n\n", result)
 		})
 	}
 }
