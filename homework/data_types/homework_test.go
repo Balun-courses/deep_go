bb7e27aa7c63936e69fee9dddb6c5e47ee61e0c6
russkih.kirill5 <Russkih.Kirill5@wb.ru>
2025-04-14 10:50:53 +0400

    done

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -8,8 +8,43 @@ import (
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+type uintType interface {
+	~uint16 | ~uint32 | ~uint64
+}
+
+func ReverseUint16(n uint16) uint16 {
+	return (n >> 8) | ((n & 0xff) << 8)
+}
+
+func ReverseUint32(n uint32) uint32 {
+	return (n >> 24) |
+		(((n >> 16) & 0xff) << 8) |
+		(((n >> 8) & 0xff) << 16) |
+		((n & 0xff) << 24)
+}
+
+func ReverseUint64(n uint64) uint64 {
+	return (n >> 56) |
+		(((n >> 48) & 0xff) << 8) |
+		(((n >> 40) & 0xff) << 16) |
+		(((n >> 32) & 0xff) << 24) |
+		(((n >> 24) & 0xff) << 32) |
+		(((n >> 16) & 0xff) << 40) |
+		(((n >> 8) & 0xff) << 48) |
+		((n & 0xff) << 56)
+}
+
+func ReverseBytes[T uintType](n T) T {
+	switch any(n).(type) {
+	case uint16:
+		return T(ReverseUint16(uint16(n)))
+	case uint64:
+		return T(ReverseUint64(uint64(n)))
+	case uint32:
+		return T(ReverseUint32(uint32(n)))
+	default:
+		panic("unreachable")
+	}
 }
 
 func TestСonversion(t *testing.T) {
@@ -41,7 +76,7 @@ func TestСonversion(t *testing.T) {
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			result := ToLittleEndian(test.number)
+			result := ReverseBytes(test.number)
 			assert.Equal(t, test.result, result)
 		})
 	}
